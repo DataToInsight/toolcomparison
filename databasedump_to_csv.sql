974ead69479ecc2fda9a74817408e1a2e8a27068
Gerben Oostra <gerben.oostra@TriFinance.nl>
2015-07-22 14:42:04 +0200

    Added commands to dump the tables as csv files, with headers

---
+++ databasedump_to_csv.sql
@@ -0,0 +1,4 @@
+﻿COPY matches TO 'C:\Program Files\PostgreSQL\9.3\data\matches.csv' DELIMITER ',' CSV HEADER;
+COPY persons TO 'C:\Program Files\PostgreSQL\9.3\data\persons.csv' DELIMITER ',' CSV HEADER;
+COPY products TO 'C:\Program Files\PostgreSQL\9.3\data\products.csv' DELIMITER ',' CSV HEADER;
+COPY transactions TO 'C:\Program Files\PostgreSQL\9.3\data\transactions.csv' DELIMITER ',' CSV HEADER;
